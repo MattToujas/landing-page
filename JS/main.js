fa55b496a529e0f631ac7d6d4498cf20975e2623
Matias Toujas <toujasmatias@gmail.com>
2026-03-11 11:55:16 -0300

    all navbar ready, responsive and desktop

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".responsive");
+
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+const closeMenu = document.querySelector("#closeMenu")
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("active");
+})
